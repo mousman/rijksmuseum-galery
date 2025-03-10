46e1a3b04cf6d57528b3b2fc321a22724190ce14
Moussa Dembélé <mousman@hotmail.com>
2025-03-10 15:19:44 +0100

    chore: add unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
-
+import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue(), vueDevTools()],
+  plugins: [
+    VueRouter(),
+    vue(),
+    vueDevTools(),
+    AutoImport({
+      imports: ['vue', VueRouterAutoImports],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
