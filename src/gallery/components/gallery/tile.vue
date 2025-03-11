8cf255a83e5bb260a830cbd38c265e941628bb77
Moussa Dembélé <mousman@hotmail.com>
2025-03-11 20:10:47 +0100

    feat: add theme

--- src/gallery/components/gallery/tile.vue
+++ src/gallery/components/gallery/tile.vue
@@ -1,26 +1,24 @@
 <script setup lang="ts">
-import type {ArtObject} from "@/gallery/types/gallery"
-import { useFetchArtObject } from "@/gallery/composables/gallery";
+import type { ArtObject } from '@/gallery/types/gallery'
+import { useFetchArtObject } from '@/gallery/composables/gallery'
 
-const {artObject} = defineProps<{
-  artObject:ArtObject
-  }>()
+const { artObject } = defineProps<{
+  artObject: ArtObject
+}>()
 
 const artOjbectId = toRef(artObject.objectNumber)
 
-const {isLoading,data, isError } = useFetchArtObject((artOjbectId))
-const imgSrc = computed(()=>{
-  const level4 = data.value?.levels.find((level)=>level.name==="z4")
-  return level4?.tiles?.[0].url ?? "";
+const { isLoading, data, isError } = useFetchArtObject(artOjbectId)
+const imgSrc = computed(() => {
+  const level4 = data.value?.levels.find((level) => level.name === 'z4')
+  return level4?.tiles?.[0].url ?? ''
 })
-
-watchEffect(()=>console.log(data.value))
 </script>
 <template>
   <span v-if="isLoading">loading</span>
   <span v-else-if="isError">error</span>
   <div v-else-if="data" class="gallery-tile">
-    <img v-if="imgSrc" :src="imgSrc" class="gallery-tile__image">
+    <img v-if="imgSrc" :src="imgSrc" class="gallery-tile__image" />
     <div class="gallery-tile__title">{{ artObject.title }}</div>
   </div>
 </template>
@@ -28,14 +26,14 @@ watchEffect(()=>console.log(data.value))
 .gallery-tile {
   position: relative;
   height: 180px;
-  background-color: black;
+  background-color: var(--background-color);
 
   &__title {
     position: absolute;
     left: 0;
     right: 0;
     bottom: 0;
-    background-color: yellow;
+    background-color: var(--background-color);
     overflow: hidden;
     display: -webkit-box;
     -webkit-line-clamp: 3;
@@ -44,11 +42,11 @@ watchEffect(()=>console.log(data.value))
     padding: 0 0.5rem;
   }
 
-  &__image{
+  &__image {
     width: 100%;
-    height:100%;
+    height: 100%;
     object-fit: contain;
-    object-position:center;
+    object-position: center;
   }
 }
 </style>
